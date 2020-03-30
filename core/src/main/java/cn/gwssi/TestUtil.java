a29a4928bed6549e329a86a416ed1481cb06b61e
yangning <yangning@cpms.com.cn>
2020-03-30 14:20:31 +0800

    新型项目运维实现

--- core/src/main/java/cn/gwssi/TestUtil.java
+++
@@ -1,7 +0,0 @@
-package cn.gwssi;
-
-public class TestUtil {
-    public static String tstMyMethod(){
-        return "我的返回值部分";
-    }
-}
--- utilitypage/src/main/java/com/gwssi/devops/utilitypage/UtilitypageApplication.java
+++ utilitypage/src/main/java/com/gwssi/devops/utilitypage/UtilitypageApplication.java
@@ -1,6 +1,5 @@
 package com.gwssi.devops.utilitypage;
 
-import cn.gwssi.TestUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UtilitypageApplication {
 
     public static void main(String[] args) {
-        //String rtnStr = TestUtil.tstMyMethod();
-        //System.out.println("rtnStr = "+rtnStr);
         SpringApplication.run(UtilitypageApplication.class, args);
     }
 
