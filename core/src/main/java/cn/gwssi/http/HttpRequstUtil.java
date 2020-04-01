bebac87bd1096376c6f7bf9c7cf6bafef625768b
yangning <yangning@gwssi.com.cn>
2020-04-01 12:36:15 +0800

    11

---
+++ core/src/main/java/cn/gwssi/http/HttpRequstUtil.java
@@ -0,0 +1,94 @@
+package cn.gwssi.http;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class HttpRequstUtil {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(HttpRequstUtil.class);
+    /**
+     * 登录返回CloseableHttpClient
+     *
+     * @param uri
+     * @param reqParam
+     * @return
+     * @throws IOException
+     */
+    public static CloseableHttpClient loginHttpClient(String uri, Map<String, String> reqParam) throws IOException {
+        // cookie信息自动保存在HttpClient中
+        CloseableHttpClient httpClient = HttpClients.custom().setDefaultCookieStore(new BasicCookieStore())
+                .setRedirectStrategy(new LaxRedirectStrategy()).build();
+        // 携带cookie访问登录网面
+        // 设置登录的账号与密码
+       /* HttpUriRequest login = RequestBuilder.post().setUri(uri)
+                .addParameter("j_username", "weblogic").addParameter("j_password", "G@sip02o18").build();*/
+        RequestBuilder requestBuilder = RequestBuilder.post().setUri(uri);
+        String successKeyword = "!@#$%^&*()";
+        for (String key : reqParam.keySet()) {
+            if("success_keyword".equals(key)){
+                successKeyword = reqParam.get(key);
+                continue;
+            }
+            requestBuilder.addParameter(key, reqParam.get(key));
+        }
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(30000).setConnectTimeout(30000).build();
+        requestBuilder.setConfig(requestConfig);
+        HttpUriRequest login = requestBuilder.build();
+        // httpclient访问登录网页,并得到响应对象
+        CloseableHttpResponse response = httpClient.execute(login);
+        // 响应文本
+        String content = EntityUtils.toString(response.getEntity());
+        //log.info(content);
+        if(!content.contains(successKeyword)){
+            throw new IOException("登陆系统异常!");
+        }
+        EntityUtils.consume(response.getEntity());
+        // 输出响应页面源代码
+        //System.out.println(content);
+        return httpClient;
+    }
+
+    public static String sessionRequest(CloseableHttpClient closeableHttpClient, String uri, Map<String, String> reqParam) throws IOException {
+        HttpPost httpPost = new HttpPost(uri);
+        // 设置请求和传输超时时间
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(30000).setConnectTimeout(30000).build();
+        httpPost.setConfig(requestConfig);
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(getParam(reqParam), "UTF-8");
+        httpPost.setEntity(entity);
+        CloseableHttpResponse response = closeableHttpClient.execute(httpPost);
+        HttpEntity httpEntity = response.getEntity();
+        return EntityUtils.toString(httpEntity, "UTF-8");
+    }
+
+    public static List<NameValuePair> getParam(Map parameterMap) {
+        List<NameValuePair> param = new ArrayList<NameValuePair>();
+        Iterator it = parameterMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry parmEntry = (Map.Entry) it.next();
+            param.add(new BasicNameValuePair((String) parmEntry.getKey(),
+                    (String) parmEntry.getValue()));
+        }
+        return param;
+    }
+}
---
+++ core/src/main/java/cn/gwssi/util/Md5Util.java
@@ -0,0 +1,92 @@
+package cn.gwssi.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class Md5Util {
+	/**
+	 * 获取字符串MD5码
+	 * @param data
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static String md5Code(String data)  {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return "";
+		}
+		md.update(data.getBytes());
+		StringBuffer buf = new StringBuffer();
+		byte[] bits = md.digest();
+		for (int i = 0; i < bits.length; i++) {
+			int a = bits[i];
+			if (a < 0)
+				a += 256;
+			if (a < 16)
+				buf.append("0");
+			buf.append(Integer.toHexString(a));
+		}
+		return buf.toString();
+	}
+	
+	/**
+     * 对一个文件获取md5值
+     * @return md5串
+	 * @throws NoSuchAlgorithmException 
+     */
+    public static String getMD5(String filePath)  {
+    	File file = new File(filePath);
+    	if(file == null || !file.exists()){
+    		return "";
+    	}
+        FileInputStream fileInputStream = null;
+        try {
+        	fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[8192];
+            int length;
+            MessageDigest newMd5 = null;
+			try
+			{
+				newMd5 = MessageDigest.getInstance("MD5");
+			}
+			catch (NoSuchAlgorithmException e)
+			{
+				e.printStackTrace();
+			}
+			
+            while ((length = fileInputStream.read(buffer)) != -1) {
+            	newMd5.update(buffer, 0, length);
+            }
+
+            return new String(Hex.encodeHex(newMd5.digest()));
+            
+        } catch (FileNotFoundException e) {
+        	e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+        	e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (fileInputStream != null){
+                	fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+		System.out.println(Md5Util.md5Code("greatwall"));
+	}
+}
