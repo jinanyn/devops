200cf0ed80542164ac5cceb8633e2a282ad05c7b
yangning <yangning@gwssi.com.cn>
2020-04-15 17:10:45 +0800

    更新

--- utilitypage/src/main/java/com/gwssi/devops/utilitypage/controller/WeblogicHealthController.java
+++ utilitypage/src/main/java/com/gwssi/devops/utilitypage/controller/WeblogicHealthController.java
@@ -8,6 +8,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
