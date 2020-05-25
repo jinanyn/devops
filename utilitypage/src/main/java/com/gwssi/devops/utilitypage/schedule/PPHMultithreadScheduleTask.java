1a79ee579e4dbd20b10bd242e76dc86c0e7dd315
yangning <yangning@gwssi.com.cn>
2020-05-25 15:01:27 +0800

    更新

--- utilitypage/src/main/java/com/gwssi/devops/utilitypage/schedule/PPHMultithreadScheduleTask.java
+++ utilitypage/src/main/java/com/gwssi/devops/utilitypage/schedule/PPHMultithreadScheduleTask.java
@@ -1,5 +1,6 @@
 package com.gwssi.devops.utilitypage.schedule;
 
+import cn.gwssi.xml.XmlHelerBuilder;
 import com.gwssi.devops.utilitypage.config.PathConfig;
 import com.gwssi.devops.utilitypage.mail.MailHelperBuilder;
 import com.gwssi.devops.utilitypage.model.RtnData;
@@ -32,5 +33,45 @@ public class PPHMultithreadScheduleTask {
     //@Scheduled(cron = "0 48 14 * * ?")
     public void pphSinkBottomCase() {//PPH沉底案件监控
         UtilityServiceInvoke.commonBizMonitorProcess(pathConfig, BusinessConstant.BIZ_PPH_SINK_BOTTOM_CASE, "pphSinkBottomCase",mailHelperBuilder);
+    }
+
+    @Async
+    //@Scheduled(cron = "0 40 23 * * ?")// 每天晚上23:40触发
+    @Scheduled(cron = "0 52 14 * * ?")// 每天晚上23:45触发
+    public void pphSupplementDeadlineOverdue() {//补正期限逾期监控
+        List<RtnData> rtnDataList = UtilityServiceInvoke.commonBizMonitorProcess(pathConfig, BusinessConstant.BIZ_PPH_SUPPLEMENT_DEADLINE_OVERDUE, "pphSupplementDeadlineOverdue");
+        if(rtnDataList != null && rtnDataList.size() >0){
+            StringBuilder slhBui = new StringBuilder();
+            StringBuilder pphidBui = new StringBuilder();
+            boolean firstFlag = true;
+            for(RtnData rtnData : rtnDataList){
+                if(firstFlag){
+                    firstFlag = false;
+                }else{
+                    slhBui.append(",");
+                    pphidBui.append(",");
+                }
+                slhBui.append(rtnData.getQixianslh());
+                pphidBui.append(rtnData.getPphid());
+            }
+            String jsonObj="{qixianslh:\""+slhBui.toString()+"\",pphid:\""+pphidBui.toString()+"\"}";
+            List<RtnData> bizDataList = UtilityServiceInvoke.commonBizHandleProcess(pathConfig,BusinessConstant.BIZ_PPH_SUPPLEMENT_DEADLINE_OVERDUE,jsonObj,"jsonObj");
+            bizDataList.parallelStream().forEach(rtnData->{
+                String bizXmlData = XmlHelerBuilder.convertObjectToXml(rtnData);
+                boolean flag = true;
+                //log.info("****************bizXmlData="+bizXmlData);
+                if (bizXmlData.indexOf("<rtnData/>") != -1) {//空白数据
+                    flag = false;
+                }
+                int idx = bizXmlData.indexOf("<rtnData>");
+                if (idx == -1) {
+                    flag = false;
+                }
+                if(flag){
+                    mailHelperBuilder.sendSimpleMessage("PPH补正期限逾期数据需要处理","待处理数据生成语句："+rtnData.getText());
+                }
+            });
+        }else{
+        }
     }
 }
