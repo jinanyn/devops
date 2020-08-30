9ae889d5c31e026539ebe1bb6f3895ccf98add26
yangning <yangning@gwssi.com.cn>
2020-08-30 12:45:19 +0800

    更新

--- utilitypage/src/main/java/com/gwssi/devops/utilitypage/schedule/UtilityMultithreadScheduleTask.java
+++ utilitypage/src/main/java/com/gwssi/devops/utilitypage/schedule/UtilityMultithreadScheduleTask.java
@@ -258,22 +258,20 @@ public class UtilityMultithreadScheduleTask {
 
     @Async
     @Scheduled(cron = "0 25 2 * * ?")// 每天上午2:20触发
-    //@Scheduled(cron = "0 08 15 * * ?")// 每天上午1:05触发
+    //@Scheduled(cron = "0 27 12 * * ?")// 每天上午1:05触发
     public void priorityApplyNationBestUnwithdraw() {//在先申请该国优视撤未国优视撤
         List<RtnData> rtnDataList = UtilityServiceInvoke.commonBizMonitorProcess(pathConfig, BusinessConstant.BIZ_PRIORITY_APPLY_NATION_BEST_UNWITHDRAW, "priorityApplyNationBestUnwithdraw",mailHelperBuilder);
         if(rtnDataList != null && rtnDataList.size() >0){
             StringBuilder sqhBui = new StringBuilder();
-            boolean firstFlag = true;
+            StringBuilder ridBui = new StringBuilder();
             for(RtnData rtnData : rtnDataList){
-                if(firstFlag){
-                    firstFlag = false;
-                }else{
-                    sqhBui.append(",");
-                }
-                sqhBui.append(rtnData.getZaixiansqh());
+                sqhBui.append(","+rtnData.getZaixiansqh());
+                ridBui.append(","+rtnData.getRid());
             }
-            UtilityServiceInvoke.commonBizHandleProcess(pathConfig,BusinessConstant.BIZ_PRIORITY_APPLY_NATION_BEST_UNWITHDRAW,sqhBui.toString(),"shenqingh");
+            //UtilityServiceInvoke.commonBizHandleProcess(pathConfig,BusinessConstant.BIZ_PRIORITY_APPLY_NATION_BEST_UNWITHDRAW,sqhBui.toString(),"shenqingh");
             //mailHelperBuilder.sendSimpleMessage("发明案源gl_yxsc_ajscb表数据重复处理","本次共处理"+sqhBui.toString()+",请核查是否正常!!!");
+            String jsonObj="{is_exec:\"true\",shenqingh:\""+sqhBui.toString().replaceFirst(",", "")+"\",rid:\""+ridBui.toString().replaceFirst(",", "")+"\"}";
+            UtilityServiceInvoke.commonBizHandleProcess(pathConfig,BusinessConstant.BIZ_PRIORITY_APPLY_NATION_BEST_UNWITHDRAW,jsonObj,"jsonObj");
         }else{
             //mailHelperBuilder.sendSimpleMessage("发明案源gl_yxsc_ajscb表数据重复处理","本次未发现需要处理的数据!!!");
         }
